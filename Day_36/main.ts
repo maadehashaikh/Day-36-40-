d482db7cb638eb001769a71fd4b781d505bd58f8
Maadeha shaikh <maadeham@gmail.com>
2024-04-25 23:09:20 +0500

    first commit

---
+++ Day_36/main.ts
@@ -0,0 +1,17 @@
+// so for the day 51, you've to read the following Article and understand everything else in it.
+// Why you should completely read it?
+// Many students ask that how to build logic in programming, how to understand the things, etc. so this article is the answer of most of your questions in simple words.
+// Top 10 Programming Tips For Beginners👀
+// https://www.geeksforgeeks.org/programming-tips-for-beginners/
+
+// Here are top 10 tips that will be useful for every coder who faces challenges during this journey !
+// 1_ Getting Theory Only But Not Practical => means many of us try to make notes of the language we are trying to learn but the case is different in learning programming and it is only practice which makes you be the master of it not making notes or just getting theory concepts . Get your hands dirty in programming and don’t skip the question in exercises thinking that it’s easy so no need to write the program. \
+
+// 2_ Don't Leave A Gap In Learnings => Learning programming is something different we learn on everyday bases , think to become a good programmer you have to daily face the issues and daily know and see the syntax that will help your brain to familiar with language soon and solving problems reduces the response time of your brain which helps in getting the logic quickly , leaving programming in between learning can break the rythm and consistency of the code which leads to breaking of learnings 
+
+// 3_ Don’t Do Over Analysis Of The Concept => Most of the students or newbie programmers overanalysis of concepts in programming which is another mistake. They don’t move to the next chapter and waste their time on a single concept. Having a good command of a specific topic is good but overanalysis can make you the worst programmer and your progress will be slow. Analyzing a concept in-depth for the first time is not good, once you have enough experience you can go in-depth for the specific concept. 
+
+// 4_ Read The Error Message Carefully => A lot of beginners don’t pay attention to the errors given on the screen and ask for help from their mentors or senior programmers immediately. Understand that errors on the screen say a lot about the issues and you can find the solution from there as well if you read the error message carefully. 
+
+// 5_ Solve MCQ's Questions =>  If you are learning to program for the first time solve MCQs as much as you can. Making a program develops logical thinking ability but solving the MCQs question helps in making good commands on syntax and you start understanding some important rules in programming.
+
